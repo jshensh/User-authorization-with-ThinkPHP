4a95478f9656ebab8530993d19d70c2905c4bd87
jshensh <admin@imjs.work>
2020-03-31 01:32:42 +0800

    路由规则更新

--- route/route.php
+++ route/route.php
@@ -14,13 +14,14 @@ Route::resource('api/:version/admin/UserGroup', 'api/:version.admin.UserGroup');
 
 Route::group('api/:version', function() {
     Route::get('admin/:controller/:action', 'api/:version.admin.:controller/:action');
+    Route::post('admin/:controller/:action', 'api/:version.admin.:controller/:action');
     Route::get('admin/:controller', 'api/:version.admin.:controller/index');
     Route::get(':controller/:action', 'api/:version.:controller/:action');
+    Route::post(':controller/:action', 'api/:version.:controller/:action');
     Route::get(':controller', 'api/:version.:controller/index');
 });
 
 Route::get('dashboard/user_group/permission/:id', 'dashboard/UserGroup/permission?id=:id');
-
 
 return [
 
