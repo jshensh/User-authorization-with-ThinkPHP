6a055e30c49f0504e1af13bca0f4ef07f06a14ca
jshensh <admin@imjs.work>
2020-06-14 02:54:30 +0800

    use macek/jquery-serialize-object

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -272,19 +272,3 @@ $(function() {
         activeParentUl.length && (activeParentUl.parent().addClass('active'));
     });
 });
-
-$.fn.serializeObject = function() {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
